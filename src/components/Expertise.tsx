3165a4cfd9fabbca6d16a4f6b53c4eca8a0808a1
Riley matewere <81653537+riley071@users.noreply.github.com>
2025-04-05 04:49:05 -0700

    Update Expertise.tsx

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faDocker, faPython, faFirefoxBrowser, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faReact, faFirefoxBrowser, faFacebook } from '@fortawesome/free-brands-svg-icons';
 import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
 import { faComputer, faPaintBrush } from "@fortawesome/free-solid-svg-icons";
